ee25d8ce0cd2605c67814db9c1f332dadcb6949d
lukekeum <lukekeum11@gmail.com>
2026-05-18 20:36:34 +0900

    feat(5주차): 레스토랑 타입 추가

--- week5/src/repository/restaurant.repository.ts
+++ week5/src/repository/restaurant.repository.ts
@@ -2,19 +2,25 @@ import fs from 'fs/promises';
 import { getDBPath } from '../utils/database';
 import { HttpException, Injectable } from '@nestjs/common';
 
+export interface Restaurant {
+  name: string;
+  address: string;
+  phone: string;
+}
+
 @Injectable()
 export class RestaurantRepository {
-  private async readData() {
+  private async readData(): Promise<Restaurant[]> {
     const fileData = await fs.readFile(getDBPath(), { encoding: 'utf-8' });
     console.log(fileData);
-    return JSON.parse(fileData || '[]');
+    return JSON.parse(fileData || '[]') as Restaurant[];
   }
 
-  private async writeData(data) {
+  private async writeData(data: Restaurant[]) {
     await fs.writeFile(getDBPath(), JSON.stringify(data));
   }
 
-  async create(data) {
+  async create(data: Restaurant) {
     const rdata = await this.readData();
     rdata.push(data);
     await this.writeData(rdata);
@@ -25,17 +31,17 @@ export class RestaurantRepository {
     return data;
   }
 
-  async findByPhone(phone) {
+  async findByPhone(phone: string) {
     const data = await this.readData();
     return data.find((item) => item.phone === phone);
   }
 
-  async findByName(name) {
+  async findByName(name: string) {
     const data = await this.readData();
     return data.find((item) => item.name === name);
   }
 
-  async update(name, updatedItem) {
+  async update(name: string, updatedItem: Partial<Restaurant>) {
     const data = await this.readData();
     const index = data.findIndex((item) => item.name == name);
     if (index === -1) {
@@ -50,7 +56,7 @@ export class RestaurantRepository {
     await this.writeData(data);
   }
 
-  async delete(name) {
+  async delete(name: string) {
     const data = await this.readData();
     const deletedData = data.filter((item) => item.name !== name);
     await this.writeData(deletedData);
