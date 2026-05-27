5eec6b1990d5188ee217a28ddf6d254a5697a933
lukekeum <lukekeum11@gmail.com>
2026-05-27 20:49:46 +0900

    feat(7주차): Prisma 모듈 불러오기

--- week5/src/restaurant/restaurant.service.ts
+++ week5/src/restaurant/restaurant.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRestaurantDto, UpdateRestaurantDto } from './dto/restaurant.dto';
 import { RestaurantRepository } from 'src/repository/restaurant.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RestaurantService {
-  constructor(private readonly restaurantRepository: RestaurantRepository) {}
+  constructor(
+    private readonly restaurantRepository: RestaurantRepository,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async create(createRestaurantDto: CreateRestaurantDto) {
     await this.restaurantRepository.create(createRestaurantDto);
