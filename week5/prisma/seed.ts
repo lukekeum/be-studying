2bf0f2e4d09d48951d724c6dd683b13a3c3f2593
lukekeum <lukekeum11@gmail.com>
2026-05-27 20:44:44 +0900

    feat(7주차): seed 추가

---
+++ week5/prisma/seed.ts
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env['DATABASE_URL'],
+});
+
+// initialize Prisma Client
+const prisma = new PrismaClient({
+  adapter,
+});
+
+async function main() {
+  // create two dummy articles
+  const restaurant1 = await prisma.restaurant.upsert({
+    where: { name: '봉수육' },
+    update: {},
+    create: {
+      name: '봉수육',
+      address: '경기 수원시 장안구 율전로108번길 11 1층',
+      phone: '0507-1460-0903',
+    },
+  });
+
+  const restaurant2 = await prisma.restaurant.upsert({
+    where: { name: '청년밥상' },
+    update: {},
+    create: {
+      name: '청년밥상',
+      address: '경기 수원시 장안구 서부로2136번길 10 1층',
+      phone: '0507-1307-1822',
+    },
+  });
+
+  console.log({ restaurant1, restaurant2 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
