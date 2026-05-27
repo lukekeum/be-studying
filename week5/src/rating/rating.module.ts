a26638309a0421441ce92ab6b314b041ba2a9818
lukekeum <lukekeum11@gmail.com>
2026-05-28 02:15:51 +0900

    feat(7주차): rating 라우터 추가

--- week5/src/rating/rating.module.ts
+++ week5/src/rating/rating.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RatingController } from './rating.controller';
+import { RatingService } from './rating.service';
 
-@Module({})
+@Module({
+  controllers: [RatingController],
+  providers: [RatingService]
+})
 export class RatingModule {}
